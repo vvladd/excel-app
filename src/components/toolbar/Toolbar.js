97f5eff1194c1e07dc5ea9c7cf13e926e78d3030
Vlad Kokhan <vvlad0202@gmail.com>
2020-06-10 06:50:13 +0400

    feat: toolbar template

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,38 +1,23 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {createToolbar} from './toolbar.template';
+import {$} from '@core/dom';
 
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar';
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
+      listeners: ['click'],
       ...options,
     });
   }
   toHTML() {
-    return `
-    <div class="button">
-      <i class="material-icons">format_align_left</i>
-    </div>
-
-    <div class="button">
-      <i class="material-icons">format_align_center</i>
-    </div>
-
-    <div class="button">
-      <i class="material-icons">format_align_right</i>
-    </div>
-
-    <div class="button">
-      <i class="material-icons">format_bold</i>
-    </div>
-
-    <div class="button">
-      <i class="material-icons">format_italic</i>
-    </div>
-
-    <div class="button">
-      <i class="material-icons">format_underlined</i>
-    </div>
-    `;
+    return createToolbar();
+  }
+  onClick(event) {
+    const $target = $(event.target);
+    if ($target.data.type === 'button') {
+      console.log($target.data.value);
+    }
   }
 }
