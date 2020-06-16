ce373a19baa70b73d281a15b5504700454c542f3
Vlad Kokhan <vvlad0202@gmail.com>
2020-06-16 08:34:29 +0400

    feat: implement AddTable button

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -2,20 +2,26 @@ import {Page} from '@core/Page';
 import {createStore} from '@core/createStore';
 import {rootReducer} from '@/redux/rootReducer';
 import {storage, debounce} from '@core/utils';
-import {initialState} from '@/redux/initialState';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {Header} from '@/components/header/Header';
 import {Excel} from '@/components/excel/Excel';
+import {normalizeInitialState} from '../redux/initialState';
+
+function storageName(param) {
+  return 'excel:' + param;
+}
 
 export class ExcelPage extends Page {
   getRoot() {
-    const store = createStore(rootReducer, initialState);
+    const params = this.params ? this.params : Date.now().toString();
+    const state = storage(storageName(params));
+    const store = createStore(rootReducer, normalizeInitialState(state));
 
     const stateListener = debounce((state) => {
       console.log('App State: ', state);
-      storage('excel-state', state);
+      storage(storageName(params), state);
     }, 300);
 
     store.subscribe(stateListener);
