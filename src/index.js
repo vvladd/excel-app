9db1f8701de36b1189c0f0cdc18bb31d8ae840c9
Vlad Kokhan <vvlad0202@gmail.com>
2020-06-17 08:02:48 +0400

    refactor: index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// import {Excel} from './components/excel/Excel';
-// import {Header} from './components/header/Header';
 import './scss/index.scss';
 import {Router} from './core/routes/Router';
 import {DashboardPage} from './pages/DashboardPage';
